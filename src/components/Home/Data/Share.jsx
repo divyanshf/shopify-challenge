70258fc08abffc77f6756539509f922feab24c95
Divyansh Falodiya <divyanshfalodiyaab@gmail.com>
2023-03-25 15:42:54 +0530

    fix: fixed host

--- src/components/Home/Data/Share.jsx
+++ src/components/Home/Data/Share.jsx
@@ -4,32 +4,32 @@ import { Box } from "@mui/system";
 import { useState } from "react";
 
 const ShareComponent = ({ data }) => {
-  const [open, setOpen] = useState(false);
-  const copy = () => {
-    navigator.clipboard.writeText(
-      `https://divyanshfalodiya.github.io/shopify-challenge/?search=${data.nasa_id}`
+    const [open, setOpen] = useState(false);
+    const copy = () => {
+        navigator.clipboard.writeText(
+            `https://divyanshf.github.io/shopify-challenge/?search=${data.nasa_id}`
+        );
+        handleOpen();
+    };
+    const handleOpen = () => setOpen(true);
+    const handleClose = () => setOpen(false);
+    return (
+        <Box>
+            <Tooltip title="Share">
+                <IconButton onClick={copy}>
+                    <Share />
+                </IconButton>
+            </Tooltip>
+            <Snackbar
+                open={open}
+                color="success"
+                message="Link copied to clipboard"
+                onClose={handleClose}
+                autoHideDuration={5000}
+                anchorOrigin={{ vertical: "bottom", horizontal: "left" }}
+            />
+        </Box>
     );
-    handleOpen();
-  };
-  const handleOpen = () => setOpen(true);
-  const handleClose = () => setOpen(false);
-  return (
-    <Box>
-      <Tooltip title="Share">
-        <IconButton onClick={copy}>
-          <Share />
-        </IconButton>
-      </Tooltip>
-      <Snackbar
-        open={open}
-        color="success"
-        message="Link copied to clipboard"
-        onClose={handleClose}
-        autoHideDuration={5000}
-        anchorOrigin={{ vertical: "bottom", horizontal: "left" }}
-      />
-    </Box>
-  );
 };
 
 export default ShareComponent;
